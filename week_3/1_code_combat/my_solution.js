2247551c4ae0a9ee401c193e75493ea2f7c14e4e
quytx1102 <quytx1102@gmail.com>
2014-05-11 14:10:14 -0500

    update mission 1

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -1,4 +1,4 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge by myself
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
@@ -14,3 +14,12 @@
  
 // Reflection:
 // Write your reflection here.
+
+// 1. Rescue Mission
+move down x2;
+move right x1;
+move up x2;
+move right x2;
+move down x1;
+attack the enemy;
+
